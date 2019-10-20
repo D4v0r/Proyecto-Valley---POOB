bcd94b6734ecc3be2c9b76b88cf6ae7e29b88fb3
D4v0r <davorcortes.dc@gmail.com>
2019-10-19 21:20:14 -0500

    Temporal

--- valley/Temporal.java
+++ valley/Temporal.java
@@ -1,21 +1,47 @@
-
+import java.util.*;
+import java.lang.*;
+import javax.swing.*;
+import shapes.*;
 /**
- * Write a description of class Temporal here.
+ * Tarp Temporal, este tipo de lona no permite hacer huecos y su visibilidad es temporal.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Davor Cortés - David Otalora 
+ * @version 1
  */
 public class Temporal extends Tarp
 {
-
-
+    private final int timeToLive;
+    
     /**
      * Constructor for objects of class Temporal
      */
     public Temporal(int []lowerEnd, int []higherEnd)
     {
         super(lowerEnd, higherEnd );
+        timeToLive = 5000;
     }
+    
+    @Override
+    public void makeVisible(){
+        super.makeVisible();
+        try{
+            Thread.sleep(timeToLive);
+        } catch (Exception e){
+            // ignoring exception at the moment
+        }
+        makeInvisible();
+    }
+    
+    @Override
+    public void makePuncture(int x){
+    }
+    
+    @Override
+    public void patchPuncture(int x){
+    }
+    
+    
 
 
 }
+
